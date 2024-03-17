b8e08cfb4073957307cc2cfd5d4552b93c7e2582
Nagakburos <wansanice@proton.me>
2024-03-17 19:21:04 -0300

    updates

--- app/(dashboard)/stats/page.tsx
+++ app/(dashboard)/stats/page.tsx
@@ -1,6 +1,9 @@
-import { getStatsAction } from "@/utils/actions";
-function StatsPage() {
-  getStatsAction();
+import { getStatsAction, getChartsDataAction } from "@/utils/actions";
+import { log } from "console";
+async function StatsPage() {getStatsAction();
+  const charts = await getChartsDataAction();
+  console.log(charts);
+
     return <h1 className='text-4xl'>StatsPage</h1>;  
   };
   export default StatsPage;
